5b879af715fb856ec3fe1ee45d2d9d7720c0a687
Ingjaguar <ing.jaguarin@gmail.com>
2022-08-23 18:48:24 -0500

    S0203 Modelado de la Clase MovimientoDinero
    S0203Modelar el movimiento de dinero como una clase de Java:
    
    Definition done:
    -Es posible crear una nueva instancia de la clase "MovimientoDinero"
    -Es posible leer y modiﬁcar el monto del movimiento
    -Es posible crear montos positivos y negativos
    -Es posible leer y modiﬁcar el concepto del movimiento
    -Es posible deﬁnir qué usuario fue encargado de registrar el movimiento

---
+++ MovimientoDinero.java
@@ -0,0 +1,38 @@
+package Sprint2;
+
+public class MovimientoDinero extends Empleado {
+    private double monto;
+    private String conceptoMovimiento;
+
+    public MovimientoDinero() {
+    }
+
+    public MovimientoDinero(double monto, String conceptoMovimiento) {
+        this.monto = monto;
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "MovimientoDinero {" +
+                "monto=" + monto +
+                ", conceptoMovimiento='" + conceptoMovimiento + '\'' +
+                '}';
+    }
+}
