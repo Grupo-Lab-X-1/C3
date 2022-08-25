390e03bf8a2fb1961bb5784b59d160bed50a3ba0
Stiwarth Vega <stewartvega99@gmail.com>
2022-08-25 17:49:26 -0500

    Se actualiza documento con la clase de movimeinto de dinero

--- Modelado de clases.java
+++ Modelado de clases.java
@@ -1,1 +1,39 @@
  /* Creacion inicial de documento */
+ package Sprint2;
+
+public class MovimientoDinero extends Empleado {
+    private double monto;
+    private String conceptoMovimiento;
+
+    public MovimientoDinero() {
+    }
+
+    public MovimientoDinero(double monto, String conceptoMovimiento) {
+        this.monto = monto;
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "MovimientoDinero {" +
+                "monto=" + monto +
+                ", conceptoMovimiento='" + conceptoMovimiento + '\'' +
+                '}';
+    }
+}
